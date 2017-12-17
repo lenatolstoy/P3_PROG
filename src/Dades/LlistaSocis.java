c1779db1108902d3c9e34205a7e69fe6227ea9f1
Toni Torres <torrescabero@gmail.com>
2017-12-17 15:51:10 +0100

    LA UNIÓN

--- src/Dades/LlistaSocis.java
+++ src/Dades/LlistaSocis.java
@@ -1,14 +1,3 @@
-/**
- * Practica 3. Classe LlistaSocis.
- *
- *
- * @author Ivan Grima
- * @author Cristina Llort
- * @author Magdalena Tolstoy
- * @author Antonio Torres
- *
- */
-
 package Dades;
 
 import java.io.BufferedWriter;
@@ -28,16 +17,28 @@ public class LlistaSocis {
 	private int lineas;
 
 	// constructor
+	/**
+	 * Constructor que crea una lista
+	 * @param len
+	 */
 	public LlistaSocis(int len) {
 		lista = new Soci[len];
 		lineas = 0;
 	}
-
+	
+	/**
+	 * Retorna el numero de socis de la lista
+	 * @return
+	 */
 	public int getLineas() {
 		return lineas;
 	}
 
 	@Override
+	/**
+	 * Metode ToString
+	 * @return string
+	 */
 	public String toString() {
 		String r = "";
 		for (int i = 0; i < lineas; i++) {
@@ -46,25 +47,22 @@ public class LlistaSocis {
 		return r;
 	}
 
-	/** HACERLO POR ORDEN ALFABETICO + LISTA DINAMICA */
+
+	/**
+	 * Afegeir un soci a la llista
+	 * @param nuevo
+	 */
 	public void afegeix(Soci nuevo) {
-		if (lineas < lista.length) {
+		if (lineas <= lista.length) {
 			lista[lineas] = nuevo;
 			lineas++;
 		}
-		else{
-			Soci[] aux = new Soci[lineas + 1];
-			for (int i = 0; i < lineas; i++) {
-
-				aux[i] = lista[i];
-			}
-
-			aux[lineas] = nuevo.Duplicat();
-			lineas++;
-			lista = aux;
-		}
 	}
-
+	
+	/**
+	 * Guarda els socis al un fitxer
+	 * @throws IOException
+	 */
 	public void guardar() throws IOException {
 		BufferedWriter escriure = new BufferedWriter(new FileWriter("user.txt"));
 		String dni;
@@ -107,7 +105,35 @@ public class LlistaSocis {
 		escriure.close();
 	}
 
+		/**
+		 * llegeix un fitxer i omple una llista de socis
+		 * @throws FileNotFoundException
+		 */
 	public void leer() throws FileNotFoundException {
+<<<<<<< HEAD
+		Scanner scan = new Scanner(new File("user.txt"));
+		String dni;
+		String nom;
+		Date data_naixement;
+		Date data_alta;
+		int incidencias;
+		int num_prestec;
+		String punts = null;
+		scan.useDelimiter("\\*");
+		while (scan.hasNext()) {
+			dni = scan.next();
+			dni = dni.replaceAll("[\n\r]", "");
+			nom = scan.next();
+			data_naixement = new Date(scan.next());
+			data_alta = new Date(scan.next());
+			incidencias = Integer.parseInt(scan.next());
+			num_prestec = Integer.parseInt(scan.next());
+			if (scan.hasNextInt()) {
+				punts = scan.next();
+			} else {
+				punts = null;
+			}
+=======
 		File f = new File("user.txt");
 		if (f.exists() && !f.isDirectory()) {
 			Scanner scan = new Scanner(f);
@@ -156,13 +182,30 @@ public class LlistaSocis {
 					Soci nuevo = new Soci(dni, nom, data_n, data_a, incidencias, num_prestec);
 					afegeix(nuevo);
 				}
+>>>>>>> 6a4fa2da781d379ffb2c9a3ee0d91a82d5c447d2
 
+			if (punts != null) {
+				int p = Integer.parseInt(punts);
+				NoEstudiant nuevo = new NoEstudiant(dni, nom, data_naixement, data_alta, incidencias, num_prestec, p);
+				afegeix(nuevo);
+			} else {
+				Soci nuevo = new Soci(dni, nom, data_naixement, data_alta, incidencias, num_prestec);
+				afegeix(nuevo);
 			}
-			scan.close();
+
 		}
+<<<<<<< HEAD
+		scan.close();
+=======
 
+>>>>>>> 6a4fa2da781d379ffb2c9a3ee0d91a82d5c447d2
 	}
-
+	
+	/**
+	 * retorna un soci a partir de un dni
+	 * @param dni
+	 * @return soci
+	 */
 	public Soci retornaSoci(String dni) {
 		for (int i = 0; i < lineas; i++) {
 			if (lista[i].getDNI().equals(dni)) {
@@ -171,12 +214,21 @@ public class LlistaSocis {
 		}
 		return null;
 	}
-
+	
+	/**
+	 * retorna la llista de socis
+	 * @return lista
+	 */
 	public Soci[] getLista() {
 		return lista;
 	}
 
-	/** MEJOR CERCA DICOTOMICA ? */
+
+/**
+ * compueba si un socio existe en la lista a partir de su dni
+ * @param dni
+ * @return
+ */
 	public boolean existeix(String dni) {
 		boolean r = false;
 		for (int i = 0; i < lineas; i++) {
@@ -187,6 +239,10 @@ public class LlistaSocis {
 		return r;
 	}
 
+	/**
+	 * Elimina un socio de la lista a apartir del dni
+	 * @param dni
+	 */
 	public void eliminar(String dni) {
 		int i = 0;
 		int posicion;
@@ -205,5 +261,6 @@ public class LlistaSocis {
 			i++;
 		} while (i < lineas && !trobat);
 	}
+
 
 }
--- src/Dades/Soci.java
+++ src/Dades/Soci.java
@@ -1,14 +1,3 @@
-/**
- * Practica 3. Classe Soci.
- *
- *
- * @author Ivan Grima
- * @author Cristina Llort
- * @author Magdalena Tolstoy
- * @author Antonio Torres
- *
- */
-
 package Dades;
 
 import java.util.Date;
@@ -24,6 +13,13 @@ public class Soci {
 	protected int num_prestec;
 
 	// constructor
+	
+	/**
+	 * Primer constructor
+	 * @param DNI
+	 * @param nom
+	 * @param data_naixement
+	 */
 	public Soci(String DNI, String nom, Date data_naixement) {
 		this.DNI = DNI;
 		this.nom = nom;
@@ -33,6 +29,15 @@ public class Soci {
 		num_prestec = 0;
 	}
 
+	/**
+	 * Segon constructor
+	 * @param DNI
+	 * @param nom
+	 * @param data_naixement
+	 * @param data_alta
+	 * @param incidencias
+	 * @param num_prestec
+	 */
 	public Soci(String DNI, String nom, Date data_naixement, Date data_alta, int incidencias, int num_prestec) {
 		this.DNI = DNI;
 		this.nom = nom;
@@ -43,83 +48,122 @@ public class Soci {
 	}
 
 	// Getters i Setters
+	/**
+	 * Retorna el DNI
+	 * @return DNI
+	 */
 	public String getDNI() {
 		return DNI;
 	}
 
+	/**
+	 * Retorna el nom
+	 * @return nom
+	 */
 	public String getNom() {
 		return nom;
 	}
 
+	/**
+	 * Actualitza el nom
+	 * @param nom
+	 */
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
 
+	/**
+	 * Retorna la data de naixement
+	 * @return data_naixement
+	 */
 	public Date getData_naixement() {
 		return data_naixement;
 	}
-
+	/**
+	 * retorna la data de alta
+	 * @return data_alta
+	 */
 	public Date getData_alta() {
 		return data_alta;
 	}
 
+	/**
+	 * Modifica la data de naixement
+	 * @param data_naixement
+	 */
 	public void setData_naixement(Date data_naixement) {
 		this.data_naixement = data_naixement;
 	}
 
+	/**
+	 * Retorna el numero de incidencias
+	 * @return incidencias
+	 */
 	public int getIncidencias() {
 		return incidencias;
 	}
 
+	/**
+	 * Modifica el numero de incidencias
+	 * @param incidencias
+	 */
 	public void setIncidencias(int incidencias) {
 		this.incidencias = incidencias;
 	}
 
+	/**
+	 * Retorna el numero de prestecs
+	 * @return num_prestec
+	 */
 	public int getNum_prestecs() {
 		return num_prestec;
 	}
 
+	/**
+	 * Retorna el numero max de prestecs
+	 * @return maxNumeroPrestecs
+	 */
 	public int getNum_max_prestec() {
 		return maxNumeroPrestecs;
 	}
 
 	// Metodes
+	/**
+	 * Retorna boolean si prestecs > max
+	 * @return
+	 */
 	public boolean max_prestec() {
 		return (num_prestec >= maxNumeroPrestecs);
 	}
-
+	/**
+	 * Modifica num_prestec +1
+	 */
 	public void prestecUP() {
 		num_prestec++;
 	}
-
+	
+	/**
+	 * Modifica num_prestec -1
+	 */
 	public void prestecDown() {
 		num_prestec--;
 	}
 
 	@Override
+	/**
+	 * Metode to string
+	 * Return string
+	 */
 	public String toString() {
 		return "DNI: " + DNI + ", nom: " + nom + ", data_naixement: " + data_naixement
 				+ ", data d'alta a la biblioteca: " + data_alta + ", incidencies: " + incidencias
 				+ ", nombre de llibres que te en prestec: " + num_prestec;
 	}
-
+	/**
+	 * Modifica Incidencias +1
+	 */
 	public void incidenciaUP() {
 		incidencias++;
-	}
-
-    /**
-	 * Mètode per duplicar un soci
-	 * @return soci
-	 */
-
-	public Soci Duplicat (){
-
-		Soci aux= new Soci(this.getDNI(), this.getNom(), this.getData_naixement());
-		aux.data_alta = this.data_alta;
-		aux.incidencias = this.incidencias;
-		aux.num_prestec = this.num_prestec;
-
-		return aux;
 	}
 
 }// final de clase
